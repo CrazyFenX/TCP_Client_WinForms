ff28316e5ec77e9b6c5068b6b7eb1a09babad8b3
CrazyFenX <kattoshkin@gmail.com>
2023-05-31 01:41:31 +0700

    Добавление FileService + Фикс асинхрона

---
+++ TCP_Client_WinForms/Services/FileService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Client_WinForms.Services
+{
+    public class FileService
+    {
+        string filePath = "";
+        ZipArhiver arhiver = null;
+        SettingsJson settingsJson = null;
+
+        public FileService(string _filePath, string _engine, int _count)
+        {
+            filePath = _filePath;
+            arhiver = new ZipArhiver();
+            settingsJson = new SettingsJson(_engine, _count);
+        }
+
+        public FileInfo GetArhive()
+        {
+            var folderPath = filePath.Substring(0, filePath.LastIndexOf('\\'));
+
+            string subpath = @"program\prearchive";
+            DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            dirInfo.CreateSubdirectory(subpath);
+
+            string namePath1 = @".\start";//имя папки для архивации
+
+            string namePath = @".\archives\";//имя папки для архивации
+            string nameZip = @".\result.zip";//имя которое будет присвоено zip
+            //string nameReArchPath = @".\extract";//имя для зарархивированной папки
+
+            FileInfo fileInf = new FileInfo(filePath);
+            
+            var newFolder = dirInfo + "\\" + subpath;
+            FileInfo fileInf1 = new FileInfo(newFolder + "\\Source.blend");
+
+            if (fileInf.Exists && !fileInf1.Exists)
+            {
+                fileInf.CopyTo(newFolder + "\\Source.blend");
+                //File.Copy(filePath, newFolder + "\\Source.blend");
+            }
+
+            var finalName = folderPath + namePath + nameZip;
+
+            var dateNow = DateTime.Now;
+
+            //ZipFile.CreateFromDirectory(newFolder, nameZip + "_" + dateNow.Year + "_" + dateNow.Month + "_" + dateNow.Day + "_"  "_" + ".zip");//создать архив zip папки... с именем...
+
+            FileInfo archivefile = new FileInfo(nameZip);
+            if (!archivefile.Exists)
+            {
+                ZipFile.CreateFromDirectory(newFolder, nameZip); //создать архив zip папки... с именем...
+                archivefile = new FileInfo(nameZip);
+            }
+            
+            return archivefile;
+
+            //ZipFile.ExtractToDirectory(nameZip, nameReArchPath);//извлечь архив zip с таким именем... в такую-то папку...
+        }
+    }
+
+    public class ZipArhiver
+    {
+        public static void Arhive()
+        {
+
+        }
+    }
+
+    public class SettingsJson
+    {
+        public string RenderEngine = "";
+        public int RaysCount = 0;
+
+        public SettingsJson(string _engine, int _raysCount)
+        {
+            RenderEngine = _engine;
+            RaysCount = _raysCount;
+        }
+    }
+}
--- TCP_Client_WinForms/Views/Form1.cs
+++ TCP_Client_WinForms/Views/Form1.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using TCP_Client_Server;
+using TCP_Client_WinForms.Services;
 
 namespace TCP_Client_WinForms
 {
@@ -14,6 +15,8 @@ namespace TCP_Client_WinForms
         Thread threadTCP;
         Thread threadUDP;
 
+        
+
         public Form1()
         {
             InitializeComponent();
@@ -23,8 +26,10 @@ namespace TCP_Client_WinForms
         {
             var ip = textBoxIp.Text;
             var port = Convert.ToInt32(textBoxPort.Text);
-            tcpClient = new Client(ip, port, textBoxState, pictureBox/*, threadTCP, threadUDP*/);
-            threadTCP = new Thread(new ThreadStart(tcpClient.StartRemoteStream));
+            tcpClient = new Client(ip, port, textBoxState/*, threadTCP, threadUDP*/);
+            
+            // ВАЖНАЯ СТРОКА!!
+            //threadTCP = new Thread(new ThreadStart(tcpClient.StartRemoteStream));
         }
 
         private void startStreamButton_Click(object sender, EventArgs e)
@@ -59,7 +64,7 @@ namespace TCP_Client_WinForms
 
             try
             {
-                tcpClient = new Client(ip, port, textBoxState, pictureBox/*, threadTCP, threadUDP*/);
+                tcpClient = new Client(ip, port, textBoxState/*, pictureBox*//*, threadTCP, threadUDP*/);
                 Client.WriteInLog("TCP Connected!", textBoxState);
 
                 Height = Convert.ToInt32(textBoxHeight.Text);
@@ -85,6 +90,74 @@ namespace TCP_Client_WinForms
             {
                 Client.WriteInLog(ex.Message, textBoxState);
             }
+        }
+
+        private void discoectBbutton_Click(object sender, EventArgs e)
+        {
+            if (tcpClient == null)
+            {
+                Client.WriteInLog("Сервер не подключен!", textBoxState);
+                return;
+            }
+            tcpClient.ServerDisconnect();
+        }
+
+        private void sendFileButton_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+            string engine = "";
+            int raysCount = 0;
+
+            if (pathTextBox.Text != "")
+                filePath = pathTextBox.Text;
+            else
+                MessageBox.Show("Путь не заполнен!");
+
+            engine = renderEngineComboBox.Text;
+            raysCount = CountOfRaysTrackBar.Value;
+
+            FileService fileService = new FileService(filePath, engine, raysCount);
+
+            var result = fileService.GetArhive();
+
+            tcpClient.SendArchive(result.FullName);
+        }
+
+        // browse
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string path = "";
+            //var a = folderBrowserDialog1.ShowDialog();
+            //if (a == DialogResult.OK)
+            //    path = folderBrowserDialog1.SelectedPath;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+                pathTextBox.Text = ofd.FileName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browseOutputButton_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            var a = folderBrowserDialog1.ShowDialog();
+            if (a == DialogResult.OK)
+                path = folderBrowserDialog1.SelectedPath;
+            OutPutFolderTextBox.Text = path;
+        }
+
+        private void CountOfRaysTrackBar_Scroll(object sender, EventArgs e)
+        {
+            CountOfRaysTextBox.Text = CountOfRaysTrackBar.Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tcpClient.SendSyncTCP(new byte[] { 1 });
         }
     }
 }
